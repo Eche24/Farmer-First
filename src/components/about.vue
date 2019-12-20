9bfe49e57dbd8651d5a0216cd037dbd241e15bac
eche24 <spwesn@gmail.com>
2019-12-20 15:56:40 +0100

    update all files

--- src/components/about.vue
+++ src/components/about.vue
@@ -1,11 +1,9 @@
 <template>
    <v-container fluid class="pa-0 grey--text">
       <v-layout wrap align-center justify-center row fill-height class="my-3" >
-    <v-flex xs12 md3 lg3 class="">
-     <v-card flat style="background-color:#f6f6f7" height="600" >
-           <v-card-title>
-        <p class=" mb-0" style="font-size:130%; ">who we are?</p>
-           </v-card-title>
+    <v-flex xs12 sm4 md4 lg4 class="">
+     <v-card flat style="background-color:#f6f6f7" height="500" >
+        
            <v-col cols="auto">
           <v-img
             height="200"
@@ -14,21 +12,21 @@
           ></v-img>
         </v-col>
         <v-card-text>
-  
+        
+        <p class="mb-3 green--text" style="font-size:170%; "> who we are?</p>
+        
     <p style="font-family: 'Quicksand', sans-serif; font-size:100%" class="font-weight-bold">
      We are solving the global challenge of hunger, using smart local solutions and smallholder farmers are championing this movement. 
-      <br/> Leveraging your (you the investors) participation on our smart digital platform, we provide smallholder farmers with trainings on best globally accepted
+      <br/>Leveraging your (you the investors) participation on our smart digital platform, we provide smallholder farmers with trainings on best globally accepted
       practices, farm inputs such as improved seeds/machinery and a timely off-taker market for their products. <br/> 
     </p>
 
         </v-card-text>
          </v-card>  
       </v-flex>
-       <v-flex xs12 md3 lg3 class="">
-         <v-card flat style="background-color:#f6f6f7"  height="600">
-           <v-card-title>
-        <p class="" style="font-size:130%;">mission</p>
-           </v-card-title>
+       <v-flex xs12 sm4 md4 lg4 class="">
+         <v-card flat style="background-color:#f6f6f7"  height="500">
+        
            <v-col cols="auto">
           <v-img
             height="200"
@@ -37,7 +35,9 @@
           ></v-img>
         </v-col>
         <v-card-text>
-    
+         
+        <p class=" green--text mb-3" style="font-size:170%;"> mission</p>
+          
     <p style="font-family: 'Quicksand', sans-serif; font-size:100%" class="font-weight-bold">
      To sustainably leverage the use of technology in driving for a 
      food sufficient continents in manners that benefit all, and in ways that protect our ecosystem.
@@ -45,11 +45,9 @@
         </v-card-text>
          </v-card>  
       </v-flex>
-       <v-flex xs12 md3 lg3 class="">
-         <v-card flat style="background-color:#f6f6f7"  height="600">
-           <v-card-title>
-        <p class="" style="font-size:130%;">Our vision</p>
-           </v-card-title>
+       <v-flex xs12 sm4 md4 lg4 class="">
+         <v-card flat style="background-color:#f6f6f7"  height="500">
+         
            <v-col cols="auto">
           <v-img
             height="200"
@@ -58,7 +56,9 @@
           ></v-img>
         </v-col>
         <v-card-text>
-    
+         
+        <p class="mb-3 font-weight-bolder green--text" style="font-size:170%;" >Our vision</p>
+          
     <p style="font-family: 'Quicksand', sans-serif; font-size:100%" class="font-weight-bold">
     Our vision is to see a food sufficient society that allows everyone on the African continent to have consistent access to healthy, affordable and nutritious food products.
     </p>
