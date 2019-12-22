d20bfd0d6d6f182a822d63d1eb749aecc40d52e3
eche24 <spwesn@gmail.com>
2019-12-22 02:50:53 +0100

    update all files

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -2,81 +2,42 @@
   <v-app>
     <nav>
       <v-app-bar
-        height="55px"
+        height="75px"
         scroll-off-screen
         app
-        dense
+        
       >
         <v-app-bar-nav-icon
           class="hidden-sm-and-up"
           @click.stop="drawer = !drawer"
         />
-        <!-- </v-btn> -->
+        
         <v-toolbar-title
           class="ml-2 pl-2"
           :to="$url('/')"
           title="Home"
         >
-          <!-- <v-img 
-            :src="require('@/assets/img/logo/logo4.png')"
-              :lazy-src="require('@/assets/img/logo/logo4.png')"
-              width="10vh"
-              height="50px"
-              >
-               
-              </v-img>
-          -->
-          <g-image src="@/assets/img/logo/logo.png" width="100vh" alt='farmer first logo'/>
+          <g-image src="@/assets/img/logo/logo3.png"  width="150vh" alt='farmer first logo' style="margin: 10px 0 0 0"/>
         </v-toolbar-title>
         <v-spacer />
         <v-toolbar-items
           class="hidden-xs-only"
+          v-for="(item, i) in items"
+          :key="i"
         >
+        
           <v-btn
+          
             text
-            :to="$url('/')"
+            :to='item.link'
             style="text-transform: capitalize;"
             flat
             class="ml-0"
+            color="primary"
           >
-            Home
+            {{item.text}}
           </v-btn>
-          <v-btn
-            text
-            :to="$url('/about')"
-            style="text-transform: capitalize;"
-            flat
-            class="ml-0"
-          >
-            About
-          </v-btn>
-          <!-- <v-btn
-            text
-            :to="$url('/team')"
-            style="text-transform: capitalize;"
-            flat
-            class="ml-0"
-          >
-            Our team
-          </v-btn> -->
-          <v-btn
-            text
-            :to="$url('/Farms')"
-            style="text-transform: capitalize;"
-            flat
-            class="ml-0"
-          >
-            Farms
-          </v-btn>
-          <v-btn
-            text
-            :to="$url('/contact')"
-            style="text-transform: capitalize;"
-            flat
-            class="ml-0"
-          >
-            Contact
-          </v-btn>
+          
         </v-toolbar-items>
       </v-app-bar>
     
@@ -87,77 +48,29 @@
       >
         <v-list-item>
           <v-list-item-content>
-            <v-list-item-title class="title blue--text pl-5">
-             <g-image src="@/assets/img/logo/logo.png" width="100vh alt='farmer first logo' "/>
+            <v-list-item-title class="">
+             <g-image src="@/assets/img/logo/logo3.png" width="100vh alt='farmer first logo' "/>
             </v-list-item-title>
           </v-list-item-content>
         </v-list-item>
         <v-divider></v-divider>
-        <v-list dense nav>
-        <v-list-item 
+          <v-list dense app>
+      <v-list-item-group v-model="item" color="primary">
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to='item.link'
         >
-         
+          <v-list-item-icon>
+            <v-icon v-text="item.icon"></v-icon>
+          </v-list-item-icon>
           <v-list-item-content>
-            <v-list-item-title
-            
-            >
-            <v-btn
-            text
-            :to="$url('/')"
-            style="text-transform: capitalize;"
-            flat
-            class="ml-0"
-          >
-          <v-icon class="mr-1">home</v-icon>
-            Home
-          </v-btn>
-            
-            </v-list-item-title>
-              <v-list-item-title
-            >
-            <v-btn
-            text
-            :to="$url('/about')"
-            style="text-transform: capitalize;"
-            flat
-            class="ml-0"
-          >
-           <v-icon class="mr-1">mdi-view-dashboard</v-icon>
-            About
-          </v-btn>
-            
-            </v-list-item-title>
-              <v-list-item-title >
-            <v-btn
-            text
-            :to="$url('/services')"
-            style="text-transform: capitalize;"
-            flat
-            class="ml-0"
-          >
-             <v-icon class="mr-1">info</v-icon>
-            Services
-          </v-btn>
-            
-            </v-list-item-title>
-              <v-list-item-title 
-            >
-         
-             <v-btn
-            text
-            :to="$url('/contact')"
-            style="text-transform: capitalize;"
-            flat
-            class="ml-0"
-          >
-          <v-icon class="mr-1">mdi-dialpad</v-icon>
-            Contact
-          </v-btn>
-            
-            </v-list-item-title>
+            <v-list-item-title v-text="item.text"></v-list-item-title>
           </v-list-item-content>
         </v-list-item>
-      </v-list>
+      </v-list-item-group>
+    </v-list>
+      
       </v-navigation-drawer>
     </nav>
     <v-content class="" style="background-color:white" >
@@ -185,6 +98,13 @@
     },
     data: () => ({
       drawer: false,
+      items:[
+          { text: 'Home', icon: 'mdi-home',link:'/'},
+          { text: 'About', icon: 'mdi-view-dashboard',link:'/about'},
+            { text: 'Farms', icon: 'mdi-account',link:'/farms'},
+            { text: 'Contact', icon: 'mdi-dialpad',link:'/contact'},
+              
+      ]
     }),
   }
 </script>
